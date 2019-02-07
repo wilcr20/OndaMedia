d1c1f36de0abab150470da22bf7537c32076a022
Angelo Guerrero Medina <angelomedinabarraza@gmail.com>
2019-02-06 23:25:57 -0600

    Change structure in Settings and home.

--- OndaMedia/src/app/app.module.ts
+++ OndaMedia/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +19,9 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    StreamingMedia,
+    
   ],
   bootstrap: [AppComponent]
 })
--- OndaMedia/src/app/home/home.page.ts
+++ OndaMedia/src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { StreamingMedia, StreamingVideoOptions, StreamingAudioOptions } from '@ionic-native/streaming-media/ngx';
+import * as $ from 'jquery'
+
 
 @Component({
   selector: 'app-home',
@@ -8,13 +11,59 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  constructor(public menuCtrl: MenuController) {
+  private videoUrl = 'http://37.187.7.106/ondamusical/live.m3u8';
+  private audioUrl = 'http://streaming1.ondamusicalradio.com:8100/onradio.mp3';
+
+  private videoOptions: StreamingVideoOptions = {
+    successCallback: () => { console.log('Video played') },
+    errorCallback: (e) => { console.log('Error streaming') },
+    orientation: 'landscape',
+    shouldAutoClose: true,
+    controls: false
+  };
+
+  private audioOptions: StreamingAudioOptions = {
+    successCallback: () => { console.log('Audio played') },
+    errorCallback: (e) => { console.log('Error streaming') }
+  };
+
+  public contact = [{url:'https://www.ondamedia.es/wp-content/uploads/2019/01/alberto-herreros2.png'},
+                     {url:'https://www.ondamedia.es/wp-content/uploads/2019/01/serezade-segui.png'},
+                     {url:'https://www.ondamedia.es/wp-content/uploads/2019/01/SM.png'},
+                     {url:'https://www.ondamedia.es/wp-content/uploads/2019/01/nerea-enguidanos.png'},
+                     {url:'https://www.ondamedia.es/wp-content/uploads/2019/01/salva_balbastre.png'}];
+
+  public arrow :boolean = false;
+
+  constructor(
+    public menuCtrl: MenuController,
+    public streamingMedia: StreamingMedia) {
 
     this.menuCtrl.enable(true);
+    this.contact.reverse();
     
    }
 
   ngOnInit() {
   }
 
+  playStreamingVideo(){
+    this.streamingMedia.playVideo(this.videoUrl, this.videoOptions);
+  }
+
+  playStreamingAudio(){
+    this.streamingMedia.playAudio(this.audioUrl, this.audioOptions);
+  }
+
+  expandBox(){
+  
+
+      $('.chat-body').slideToggle('slow');
+      
+      this.arrow = !this.arrow;
+  }
+  
+
+  
+  
 }
