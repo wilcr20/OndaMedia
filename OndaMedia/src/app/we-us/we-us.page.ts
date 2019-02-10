83d89674523b8f8347e05cdabf7e50a036dc2b6e
Wilfred <wilcr20@gmail.com>
2019-02-09 21:47:56 -0600

    Bugs Fixeds

--- OndaMedia/src/app/we-us/we-us.page.ts
+++ OndaMedia/src/app/we-us/we-us.page.ts
@@ -10,17 +10,23 @@ export class WeUsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.resetPlayButtons();
   }
 
   playMusic(idM){
     console.log("Play ", idM);
+    this.resetPlayButtons();
+    //Se habilita el spinner para el boton presionado
+    document.getElementById('audio'+idM).setAttribute('src','https://gifimage.net/wp-content/uploads/2017/09/blue-loading-gif-transparent-10.gif')
+
+  }
+
+  resetPlayButtons(){
     let allBtn= document.getElementsByClassName("playButton"); // Obtiene todos los botones de la lista de canciones
     
     for (let i = 0; i < allBtn.length; i++) {  //se les resetea la imagen de play
       allBtn[i].setAttribute('src','http://www.pngmart.com/files/3/Play-Button-PNG-Image.png')   
     }
-    //Se habilita el spinner para el boton presionado
-    document.getElementById('audio'+idM).setAttribute('src','https://gifimage.net/wp-content/uploads/2017/09/blue-loading-gif-transparent-10.gif')
 
   }
 
